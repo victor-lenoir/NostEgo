bce6f26bed727cc5d98592bce6d8481d8e627456
Victor Lenoir <lenoirvictor90@gmail.com>
2011-12-11 10:31:54 +0100

    monster: monsters can now attack, move and stuff

--- game/src/map/monster.cc
+++ game/src/map/monster.cc
@@ -2,71 +2,134 @@
 #include "../option/option.hh"
 #include <iostream>
 
+enum
+{
+  LEFT,
+  DOWN,
+  UP,
+  RIGHT
+};
+
 Monster::Monster (int x, int y, std::ifstream& input, std::string hash, std::string element)
   : Element (element, x, y, 8)
 {
   animation->pause ();
-  dir = 2;
+  dir = LEFT;
   refresh_dir();
+  state = STANDING;
   set_global(hash);
 }
 
-void Monster::attack ()
+void Monster::attack (int dir_p)
 {
-
-    animation->FlipX(dir % 2);
-    if (dir < 2)
-        animation->setAnimRow(4);
-    else
-        animation->setAnimRow(5);
-    animation->loop(false);
-    animation->reset();
-    animation->play();
+    if ((state == STANDING) || (state == MOVING) || (state == RUNNING))
+    {
+        dir = dir_p;
+        state = ATTACKING;
+        refresh_dir ();
+        animation->loop(false);
+        animation->once = false;
+        animation->reset();
+        animation->play();
+    }
 }
 
 void Monster::die ()
 {
-    animation->FlipX(dir % 2);
-     if (dir < 2)
-        animation->setAnimRow(6);
-    else
-        animation->setAnimRow(7);
+    state = DYING;
+    refresh_dir ();
     animation->loop(false);
+    animation->once = true;
     animation->reset();
     animation->play();
-    animation->once = true;
     dead = true;
 }
 void Monster::refresh_dir ()
 {
     animation->FlipX(dir % 2);
-    if (dir < 2)
-        animation->setAnimRow(0);
-    else
-        animation->setAnimRow(1);
+    if (state == STANDING)
+        animation->setAnimRow(0 + (dir >= 2));
+    else if (state == MOVING)
+        animation->setAnimRow(0 + (dir >= 2));
+    else if (state == RUNNING)
+        animation->setAnimRow(2 + (dir >= 2));
+    else if (state == ATTACKING)
+        animation->setAnimRow(4 + (dir >= 2));
+     else if (state == DYING)
+        animation->setAnimRow(6 + (dir >= 2));
 }
+
+void Monster::AI ()
+{
+    move (LEFT);
+}
+
+void Monster::move (int dir_p)
+{
+    if ((state == STANDING) || (state == MOVING) || (state == RUNNING))
+    {
+        dir = dir_p;
+        if (state != MOVING)
+         animation->reset();
+        state = MOVING;
+        animation->loop(true);
+        animation->once = false;
+        animation->play();
+        if (dir_p == LEFT)
+            animation->Move (-1, 0);
+        else if (dir_p == RIGHT)
+            animation->Move (1, 0);
+        else if (dir_p == UP)
+            animation->Move (0, -1);
+        else if (dir_p == DOWN)
+            animation->Move (0, 1);
+    }
+}
+
+void Monster::hit (int deg)
+{
+    die ();
+}
+
+void Monster::state_manage ()
+{
+    if (!animation->isPlaying())
+    {
+        if (state == ATTACKING)
+            state = STANDING;
+    }
+}
+void Monster::process_keyboard_bottom ()
+{
+    if (app->GetInput().IsKeyDown (sf::Key::Space))
+        hit (12);
+    attack (DOWN);
+}
+
+void Monster::process_keyboard_top ()
+{
+    if (app->GetInput().IsKeyDown (sf::Key::Space))
+        hit (12);
+    attack (UP);
+}
+
+void Monster::process_keyboard_left ()
+{
+    if (app->GetInput().IsKeyDown (sf::Key::Space))
+        hit (12);
+    attack (LEFT);
+}
+
+void Monster::process_keyboard_right ()
+{
+    if (app->GetInput().IsKeyDown (sf::Key::Space))
+        hit (12);
+    attack (RIGHT);
+}
+
 void Monster::process_keyboard ()
 {
-    if ((app->GetInput().IsKeyDown (sf::Key::Right)))
-        {
-           dir = 1;
-           refresh_dir();
-        }
-     if ((app->GetInput().IsKeyDown (sf::Key::Left)))
-        {
-           dir = 0;
-           refresh_dir();
-        }
-       if ((app->GetInput().IsKeyDown (sf::Key::Up)))
-        {
-           dir = 2;
-           refresh_dir();
-        }
-     if ((app->GetInput().IsKeyDown (sf::Key::Down)))
-        {
-           dir = 3;
-           refresh_dir();
-        }
-    if ((app->GetInput().IsKeyDown (sf::Key::Space)))
-        die ();
+    state_manage();
+    AI ();
+    refresh_dir ();
 }
--- game/src/map/monster.hh
+++ game/src/map/monster.hh
@@ -7,15 +7,33 @@
 
 # include <fstream>
 
+enum
+{
+  STANDING,
+  MOVING,
+  RUNNING,
+  ATTACKING,
+  DYING
+};
+
 class Monster : public Element
 {
 public:
   virtual void process_keyboard ();
-  void attack ();
+  void attack (int dir_p);
   void die ();
+  void AI ();
+  void state_manage ();
+  void move (int dir_p);
+  void hit (int deg);
+  virtual void process_keyboard_bottom ();
+  virtual void process_keyboard_top ();
+  virtual void process_keyboard_left ();
+  virtual void process_keyboard_right ();
   void refresh_dir ();
   Monster (int x, int y, std::ifstream& input, std::string hash, std::string element);
   int dir;
+  int state;
   bool dead;
 };
 
