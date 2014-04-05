5b7ac4beb6c1295e9f0a8a39b11804b4c19102f8
Victor Lenoir <victor@jellynote.com>
2014-04-05 15:01:24 +0200

    fix thread problem

--- game/src/main.cc
+++ game/src/main.cc
@@ -12,7 +12,6 @@ MemoryManager<sf::Image>* img_mng;
 void listen_server (void* data);
 void listen_server (void* data)
 {
-  sf::Clock clock;
   int n;
   int id;
   int x;
@@ -22,67 +21,58 @@ void listen_server (void* data)
   int dir;
   std::string str;
   std::string world_map;
-  sf::Packet sPacket;
-
   (void)data;
-  sPacket << NETWORK_NEW_CHARACTER;
-  g->Socket.Send(sPacket);
-  while (app->IsOpened())
-  {
-    if (clock.GetElapsedTime() * 1000 > (1000 / opt->fps))
-    {
-      sf::Packet Packet;
-      int code;
-      g->Socket.Receive(Packet);
-      Packet >> code;
-      switch (code)
-      {
-      case NETWORK_NEW_MAP:
-        Packet >> world_map >> xmap >> ymap;
-        Packet >> n;
-        
-        for (std::map<int, Character*>::iterator it = g->characters.begin(); it != g->characters.end(); it++) {
-          delete it->second;
-        }
-        g->characters.clear();
-        for (size_t k = 0; k < n; ++k) {
-          Packet >> id >> x >> y;
-          // LOAD PLAYER
-          g->characters[id] = new Character(world_map, xmap, ymap, x, y);
-        }
-        g->load_map();
-        //g->player->id = n;
-        break;
-      case NETWORK_CHARACTER_MOVE:
-        Packet >> id >> x >> y >> dir;
-        g->characters[id]->x = x;
-        g->characters[id]->y = y;
-        g->characters[id]->dir = dir;
-        g->characters[id]->animation->play();
-        break;
-      case NETWORK_EXIT_MAP:
-        Packet >> n;
-        g->characters.erase(n);
-        break;
-      default:
-        std::cerr << "ERR CODE NOT HANDLE ("
-                  << code << ")"
-                  << std::endl;
-        break;
-      }
+  sf::Packet Packet;
+  int code;
 
-      clock.Reset ();
+  g->Socket.SetBlocking(false);
+  if (g->Socket.Receive(Packet) != sf::Socket::Done)
+    return;
+  Packet >> code;
+  switch (code)
+  {
+  case NETWORK_NEW_MAP:
+    Packet >> world_map >> xmap >> ymap;
+    Packet >> n;
+
+    for (std::map<int, Character*>::iterator it = g->characters.begin(); it != g->characters.end(); it++) {
+      delete it->second;
     }
-    else
-      sf::Sleep((1.0/ (float)opt->fps) - clock.GetElapsedTime());
+    g->characters.clear();
+    for (size_t k = 0; k < n; ++k) {
+      Packet >> id >> x >> y;
+      // LOAD PLAYER
+      g->characters[id] = new Character(world_map, xmap, ymap, x, y);
+    }
+    g->load_map();
+    //g->player->id = n;
+    break;
+  case NETWORK_CHARACTER_MOVE:
+    Packet >> id >> x >> y >> dir;
+    g->characters[id]->x = x;
+    g->characters[id]->y = y;
+    g->characters[id]->dir = dir;
+    g->characters[id]->animation->play();
+    break;
+  case NETWORK_EXIT_MAP:
+    Packet >> n;
+    g->characters.erase(n);
+    break;
+  default:
+    std::cerr << "ERR CODE NOT HANDLE ("
+              << code << ")"
+              << std::endl;
+    break;
   }
 }
 
 int main ()
 {
   sf::Clock clock;
-  sf::VideoMode vm = sf::VideoMode::GetDesktopMode();
-  sf::Thread tr (listen_server);
+  //sf::VideoMode vm = sf::VideoMode::GetDesktopMode();
+  sf::VideoMode vm = sf::VideoMode(800, 600);
+  sf::Packet sPacket;
+
 
   img_mng = new MemoryManager<sf::Image>;
   //app = new sf::RenderWindow(vm, "Nost ego", sf::Style::Fullscreen);
@@ -91,10 +81,14 @@ int main ()
   g = new Game;
   g->init_game ();
 
+  sPacket << NETWORK_NEW_CHARACTER;
+  g->Socket.Send(sPacket);
+
   clock.Reset ();
-  tr.Launch ();
+  //tr.Launch ();
   while (app->IsOpened())
   {
+    listen_server(0);
     if (clock.GetElapsedTime() * 1000 > (1000 / opt->fps))
     {
       app->Clear();
@@ -107,7 +101,6 @@ int main ()
     else
       sf::Sleep((1.0/ (float)opt->fps) - clock.GetElapsedTime());
   }
-  tr.Terminate ();
   delete g;
   delete opt;
   delete img_mng;
